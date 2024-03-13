eb4c3e62d96f60447c71830b9bcde10fca174170
zburns12 <zburns103@gmail.com>
2024-03-12 20:55:14 -0400

    Adding other dotfiles

---
+++ nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,15 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  lazy = false,
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('nvim-tree').setup {}
+  end,
+}
